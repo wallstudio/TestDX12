ad9626bf810aec5849ec180c5b8b095f1d9feb1c
UpHash <yukawallstudio@gmail.com>
2021-03-26 00:50:11 +0900

    WinMain

--- TestDX12/TestDX12.cpp
+++ TestDX12/TestDX12.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <windows.h>
+#include <tchar.h>
 
+// /SUBSYSTEM: Console のエントリ
 int main(int argc, char* argv[])
 {
 	std::cout << "HelloWorld";
+	return WinMain(NULL, NULL, NULL, 0);
+}
+
+// /WINDOS: WINDOWS のエントリ
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nShowCmd)
+{
 	return 0;
 }
