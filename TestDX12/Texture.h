7dd832c53e5e28593a4d7595b13a225c5208a04c
UpHash <yukawallstudio@gmail.com>
2021-04-11 06:32:47 +0900

    二段階転送

--- TestDX12/Texture.h
+++ TestDX12/Texture.h
@@ -23,9 +23,9 @@ private:
         std::byte A;
     };
     Microsoft::WRL::ComPtr<ID3D12Device8> m_Device;
-    // D3D12_HEAP_PROPERTIES m_UploadHeapProp = {};
-    // D3D12_RESOURCE_DESC m_UploadResourceDesc = {};
-    // Microsoft::WRL::ComPtr<ID3D12Resource> m_UploadResource;
+    D3D12_HEAP_PROPERTIES m_UploadHeapProp = {};
+    D3D12_RESOURCE_DESC m_UploadResourceDesc = {};
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_UploadResource;
     D3D12_HEAP_PROPERTIES m_HeapProp = {};
     D3D12_RESOURCE_DESC m_ResourceDesc = {};
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_DescriptorHeap;
@@ -48,9 +48,9 @@ public:
         // Resource確保
         m_HeapProp =
         {
-            .Type = D3D12_HEAP_TYPE::D3D12_HEAP_TYPE_CUSTOM,
-            .CPUPageProperty = D3D12_CPU_PAGE_PROPERTY::D3D12_CPU_PAGE_PROPERTY_WRITE_BACK,
-            .MemoryPoolPreference = D3D12_MEMORY_POOL::D3D12_MEMORY_POOL_L0,
+            .Type = D3D12_HEAP_TYPE::D3D12_HEAP_TYPE_DEFAULT,
+            .CPUPageProperty = D3D12_CPU_PAGE_PROPERTY::D3D12_CPU_PAGE_PROPERTY_UNKNOWN,
+            .MemoryPoolPreference = D3D12_MEMORY_POOL::D3D12_MEMORY_POOL_UNKNOWN,
             .CreationNodeMask = 0,
             .VisibleNodeMask = 0,
         };
@@ -67,9 +67,79 @@ public:
             .Layout = D3D12_TEXTURE_LAYOUT::D3D12_TEXTURE_LAYOUT_UNKNOWN,
             .Flags = D3D12_RESOURCE_FLAGS::D3D12_RESOURCE_FLAG_NONE,
         };
-        AssertOK(m_Device->CreateCommittedResource(&m_HeapProp, D3D12_HEAP_FLAGS::D3D12_HEAP_FLAG_NONE, &m_ResourceDesc, D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, nullptr, IID_PPV_ARGS(&m_Resource)));
-        // Resourceに書き込み
-        AssertOK(m_Resource->WriteToSubresource(0, nullptr, matrix.data(), static_cast<UINT>(rect.Width * sizeof(*matrix.data())), static_cast<UINT>(matrix.size() * sizeof(*matrix.data()))));
+        AssertOK(m_Device->CreateCommittedResource(&m_HeapProp, D3D12_HEAP_FLAGS::D3D12_HEAP_FLAG_NONE, &m_ResourceDesc, D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_COPY_DEST, nullptr, IID_PPV_ARGS(&m_Resource)));
+        D3D12_PLACED_SUBRESOURCE_FOOTPRINT footprint;
+        UINT64 totalSize;
+        m_Device->GetCopyableFootprints(&m_ResourceDesc, 0, 1, 0, &footprint, nullptr, nullptr, &totalSize);
+        D3D12_TEXTURE_COPY_LOCATION dest
+        {
+            .pResource = m_Resource.Get(),
+            .Type = D3D12_TEXTURE_COPY_TYPE::D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX,
+            .SubresourceIndex = 0,
+        };
+        
+        // Upload用
+        m_UploadHeapProp =
+        {
+            .Type = D3D12_HEAP_TYPE::D3D12_HEAP_TYPE_UPLOAD,
+            .CPUPageProperty = D3D12_CPU_PAGE_PROPERTY::D3D12_CPU_PAGE_PROPERTY_UNKNOWN,
+            .MemoryPoolPreference = D3D12_MEMORY_POOL::D3D12_MEMORY_POOL_UNKNOWN,
+            .CreationNodeMask = 0,
+            .VisibleNodeMask = 0,
+        };
+        m_UploadResourceDesc =
+        {
+            .Dimension = D3D12_RESOURCE_DIMENSION::D3D12_RESOURCE_DIMENSION_BUFFER,
+            .Alignment = 0,
+            .Width = totalSize,
+            .Height = 1,
+            .DepthOrArraySize = 1,
+            .MipLevels = 1,
+            .Format = DXGI_FORMAT::DXGI_FORMAT_UNKNOWN,
+            .SampleDesc = { .Count = 1, .Quality = 0, },
+            .Layout = D3D12_TEXTURE_LAYOUT::D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+            .Flags = D3D12_RESOURCE_FLAGS::D3D12_RESOURCE_FLAG_NONE,
+        };
+        AssertOK(m_Device->CreateCommittedResource(&m_UploadHeapProp, D3D12_HEAP_FLAGS::D3D12_HEAP_FLAG_NONE, &m_UploadResourceDesc, D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&m_UploadResource)));
+        void *mapping;
+        AssertOK(m_UploadResource->Map(0, nullptr, &mapping));
+        std::memcpy(mapping, matrix.data(), matrix.size() * sizeof(*matrix.data()));
+        m_UploadResource->Unmap(0, nullptr);
+        D3D12_TEXTURE_COPY_LOCATION src
+        {
+            .pResource = m_UploadResource.Get(),
+            .Type = D3D12_TEXTURE_COPY_TYPE::D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT,
+            .PlacedFootprint = footprint,
+        };
+
+        // Copy
+        ComPtr<ID3D12CommandQueue> commandQueue;
+        D3D12_COMMAND_QUEUE_DESC commandQueueCreateDesc =
+        {
+            .Type = D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT,
+            .Priority = D3D12_COMMAND_QUEUE_PRIORITY::D3D12_COMMAND_QUEUE_PRIORITY_NORMAL,
+            .Flags = D3D12_COMMAND_QUEUE_FLAGS::D3D12_COMMAND_QUEUE_FLAG_NONE,
+            .NodeMask = 0,
+        };
+        AssertOK(m_Device->CreateCommandQueue(&commandQueueCreateDesc, IID_PPV_ARGS(&commandQueue)));
+        ComPtr<ID3D12CommandAllocator> commandAllocator;
+        AssertOK(m_Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator)));
+        ComPtr<ID3D12GraphicsCommandList> commandList;
+        AssertOK(m_Device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator.Get(), nullptr, IID_PPV_ARGS(&commandList)));
+        commandList->CopyTextureRegion(&dest, 0, 0, 0, &src, nullptr);
+        ChangeBarrier(commandList.Get(), m_Resource.Get(), D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATES::D3D12_RESOURCE_STATE_GENERIC_READ);
+        commandList->Close();
+        commandQueue->ExecuteCommandLists(1, std::vector<ID3D12CommandList*>({ commandList.Get() }).data());
+        ComPtr<ID3D12Fence> fence;
+        AssertOK(m_Device->CreateFence(0, D3D12_FENCE_FLAGS::D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
+        AssertOK(commandQueue->Signal(fence.Get(), 1));
+        if (fence->GetCompletedValue() != 0)
+        {
+            auto renderFinishEvent = CreateEvent(nullptr, false, false, nullptr);
+            fence->SetEventOnCompletion(1, renderFinishEvent);
+            WaitForSingleObject(renderFinishEvent, INFINITE);
+            CloseHandle(renderFinishEvent);
+        }
 
         // Descriptor
         D3D12_DESCRIPTOR_HEAP_DESC descriptorHeapDesc =
@@ -90,6 +160,7 @@ public:
         m_Device->CreateShaderResourceView(m_Resource.Get(), &m_View, m_DescriptorHeap->GetCPUDescriptorHandleForHeapStart());
     }
 
+private:
     static std::tuple<std::vector<Pixel>, Gdiplus::Rect> LoadFromFile(std::string file)
     {
         using std::vector;
@@ -129,5 +200,22 @@ public:
             }
             cout << endl;
         }
+    }
+
+    void ChangeBarrier(Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commands, Microsoft::WRL::ComPtr<ID3D12Resource> resource, D3D12_RESOURCE_STATES prevState, D3D12_RESOURCE_STATES nextState)
+    {
+        D3D12_RESOURCE_BARRIER barrier =
+        {
+            .Type = D3D12_RESOURCE_BARRIER_TYPE::D3D12_RESOURCE_BARRIER_TYPE_TRANSITION,
+            .Flags = D3D12_RESOURCE_BARRIER_FLAGS::D3D12_RESOURCE_BARRIER_FLAG_NONE,
+            .Transition =
+            {
+                .pResource = resource.Get(),
+                .Subresource = 0,
+                .StateBefore = prevState,
+                .StateAfter = nextState,
+            },
+        };
+        commands->ResourceBarrier(1, &barrier);
     }
 };
